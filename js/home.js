54deaeae5409fd7df02f845b914f7b08f4a4f8f8
raofun_ <raofunazad851@gmail.com>
2025-12-04 22:34:15 +0600

    partially added addmoney

--- js/home.js
+++ js/home.js
@@ -1,1 +1,11 @@
 console.log("Home.js file is loaded");
+
+document.getElementById('btn-money').addEventListener('click', function(event){
+    event.preventDefault();
+    
+    const addMoney = document.getElementById('add-ad-money').value;
+    
+    const pinInput = document.getElementById('input-pin').value;
+
+    
+});
